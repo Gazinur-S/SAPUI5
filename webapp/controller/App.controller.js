fa1532b42a78f4ae7047ead62788f6384a83dc95
Gazinur Sibgatullin <ynicor@gmail.com>
2019-06-21 12:10:19 +0000

    WEEK 2 UNIT 3 AUTOMATIC CONVERSION WITH DATA TYPES

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"opensap/myapp/model/formatter"
 	
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, formatter) {
 	"use strict";
 	
 	return Controller.extend("opensap.myapp.controller.App", {
-
+		formatter : formatter,
 		onShowHello : function () {
 			// read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
